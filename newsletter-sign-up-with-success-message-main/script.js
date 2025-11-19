308fa4b0fcfe859ea23e849a2dcfe6396a4b8a65
beny25585 <binyamin25585@gmail.com>
2025-11-19 03:17:58 +0200

    finish transition form signUp to sucessesMessege and starting error style

--- newsletter-sign-up-with-success-message-main/script.js
+++ newsletter-sign-up-with-success-message-main/script.js
@@ -1,11 +1,30 @@
-const overlay = document.querySelector(".overlay");
-const closeBtn = document.querySelector(".closeBtn");
-const submitBtn = document.querySelector("button");
+const signUpBox = document.querySelector(".sign-up-box");
+const successMessege = document.querySelector(".successMessege");
+const submitBtn = document.querySelector(".submit");
+const dismissBtn = document.querySelector(".dismiss");
+const errorMassege = document.querySelector(".error-message");
+const emailInput = document.querySelector(".email");
+
+function checkValidEmail(input) {
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailPattern.test(input);
+}
 
 submitBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  overlay.classList.add("active");
+  if (checkValidEmail(emailInput.value)) {
+    e.preventDefault();
+    successMessege.classList.add("active");
+    signUpBox.classList.add("active");
+    errorMassege.classList.remove("active");
+    console.log(successMessege);
+  } else {
+    console.log(errorMassege);
+    errorMassege.classList.add("active");
+    emailInput.classList.add("active");
+  }
 });
-closeBtn.addEventListener("click", () => {
-  overlay.classList.remove("active");
+
+dismissBtn.addEventListener("click", () => {
+  successMessege.classList.remove("active");
+  signUpBox.classList.remove("active");
 });
